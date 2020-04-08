e3140a27e649f00018c1522bdc54896f1bd44eee
Sentiro <yyc.love.bb@Gmail.com>
2020-04-08 23:27:35 +0800

    habits
    apply timeline and ensure the tab switch, calendar

--- miniprogram/pages/habit/habit.js
+++ miniprogram/pages/habit/habit.js
@@ -1,25 +1,56 @@
 // miniprogram/pages/habit/habit.js
 var util = require('../../utils.js');
 Page({
+  data: {
+    TabCur: '0',
+    scrollLeft: 0,
+    date: null,
+    calendarHide:true,
+    year:0,
+    month:0,
+    clickDate: null
+  },
+  tabSelect(e) {
+    console.log(e.currentTarget.dataset.id)
+    this.setData({
+      TabCur: e.currentTarget.dataset.id,
+      scrollLeft: (e.currentTarget.dataset.id - 1) * 60,
+    })
+    /*const target = e.currentTarget.dataset.name;//1或者2得到点击了按钮1或者按钮2 
+    const url = "/pages/habit/" + target+"/"+target;//得到页面url 
+    wx.navigateTo({
+      url: url,
+    })*/
+  },
   dayClick: function (event) {
-    console.log(event.detail);
-    this.setData({ date: "Hello World" })
+    var year=event.detail.year;
+    var month = event.detail.month;
+    var day = event.detail.day;
+    this.setData({
+      date: year + "年" + month + "月" + day + "日",
+      clickDate: date
+    })
+  },
+  showCalendar(e){
+      this.setData({
+        calendarHide: !this.data.calendarHide
+      }) 
   },
   /**
    * 页面的初始数据
    */
-  data: {
-    date: null
-  },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    var time = util.formatTime(new Date());
+    var time=new Date();
+    //var time = util.formatTime(new Date());
+   /* var timestamp = Date.parse(new Date());
+    var date = new Date(timestamp);*/
     // 再通过setData更改Page()里面的data，动态更新页面的数据
     this.setData({
-      date: time
+      date: time.getFullYear()+"年"+(time.getMonth()+1)+"月"+time.getDate()+"日",
     });
   },
 
